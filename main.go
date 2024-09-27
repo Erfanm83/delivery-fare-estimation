afd17055d36d2177720f57d786b5abbe8f7d2161
Erfanm83 <erfanmahmudi1383@gmail.com>
2024-09-27 18:45:10 +0330

    haversine func added as well

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
 	fmt.Printf("Project Initialized \n")
+	fmt.Printf("haversine distance : %f\n", haversine(51.5007,
+		0.1246,
+		40.6892,
+		74.0445))
 
 }
 
-// Haversine function to calculate distance
+// haversine function to calculate distance
 func haversine(lat1, lon1, lat2, lon2 float64) float64 {
-	// Calculation code
-	return 0
+
+	// distance between latitude and longitudes
+	deltaLat := (lat2 - lat1) * math.Pi / 180.0
+	deltaLon := (lon2 - lon1) * math.Pi / 180.0
+
+	// Convert to radians
+	latRad1 := lat1 * math.Pi / 180.0
+	latRad2 := lat2 * math.Pi / 180.0
+
+	// formul
+	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
+		math.Cos(latRad1)*math.Cos(latRad2)*math.Sin(deltaLon/2)*math.Sin(deltaLon/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	radiusEarthKm := 6371.0
+
+	return radiusEarthKm * c
 }
